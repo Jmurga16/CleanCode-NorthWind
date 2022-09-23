d50512d5001c94ea5a6d78d3578aa14ea16af414
Jmurga16 <murga.jose16@gmail.com>
2022-09-22 20:41:34 -0500

    Agregar archivos de proyecto.

---
+++ NorthWind.Entities/POCOEntities/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NorthWind.Entities.POCOEntities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ NorthWind.Repositories.EFCore/DataContext/NorthWindContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.DataContext
+{
+    class NorthWindContextFactory :
+        IDesignTimeDbContextFactory<NorthWindContext>
+    {
+        public NorthWindContext CreateDbContext(string[] args)
+        {
+            var OptionBuilder = new DbContextOptionsBuilder<NorthWindContext>();
+            OptionBuilder.UseSqlServer(
+                "Server=.;Database=NorthWindDB;Trusted_Connection=True;");
+            return new NorthWindContext(OptionBuilder.Options);
+
+        }
+
+    }
+}
